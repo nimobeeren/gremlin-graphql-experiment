0a10056ea86b30589c5e80b025afc7cb6f1648e1
Nimo Beeren <nimobeeren@gmail.com>
2020-08-08 18:41:39 +0200

    graphql stuff

--- docs/links.md
+++ docs/links.md
@@ -7,3 +7,6 @@ https://tinkerpop.apache.org/docs/3.4.7/reference/#connecting-via-console
 Connecting JS to server
 https://tinkerpop.apache.org/docs/3.4.7/reference/#connecting-gremlin-server
 https://www.npmjs.com/package/gremlin
+
+Docker container for server
+https://tinkerpop.apache.org/docs/3.4.7/reference/#gremlin-server-docker-image
--- index.ts
+++ index.ts
@@ -1,12 +1,42 @@
 import { process, driver } from "gremlin";
 const { traversal } = process.AnonymousTraversalSource;
 const { DriverRemoteConnection } = driver;
+import { ApolloServer, gql } from "apollo-server";
 
 (async () => {
   const g = traversal().withRemote(
     new DriverRemoteConnection("ws://localhost:8182/gremlin")
   );
 
-  const names = await g.V().hasLabel("person").values("name").toList();
-  console.log(names);
+  const p = (await g.V(1).elementMap().next()).value;
+  console.log(p);
+  // TODO: get queried fields from resolver and pass them to elementMap()
+
+  const typeDefs = gql`
+    type Query {
+      person: Person
+    }
+
+    type Person {
+      id: ID!
+      name: String!
+      age: Int!
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      person: async () => {
+        return p;
+      },
+    },
+  };
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await server.listen();
+  console.log(`🚀 Server ready at ${url}`);
 })();
